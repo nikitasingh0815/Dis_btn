1b60884ddfdaadea4537c98ec421a8ffd6490e49
nikitasingh0815 <167523859+nikitasingh0815@users.noreply.github.com>
2025-08-20 01:00:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import DisableButton from './Disablebutton.jsx';
+//import FilterableList from './FilterableList.jsx';
 
 function App() {
   return (
     <div>
       <h1>Self-Disabling Button</h1>
+      //<h1>SEARCH FILTER LIST</h1>
       <DisableButton />
+       //<FilterableList />
     </div>
   );
 }
